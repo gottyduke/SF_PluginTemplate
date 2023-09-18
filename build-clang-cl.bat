af25586135c961af2eaaa2d8f405bd90ce30bd62
vcpkg-action <gottyduke@hotmail.com>
2023-09-18 13:40:03 +0800

    feat!: remove unneeded clang-cl support

--- build-clang-cl.bat
+++
@@ -1,4 +0,0 @@
-echo off
-rd /s /q "%~dp0/build"
-cmake -B "%~dp0/build" -S "%~dp0/Plugin" --preset=build-release-clang-cl-ninja
-cmake --build "%~dp0/build"
--- make-sln-clang-cl.bat
+++
@@ -1,3 +0,0 @@
-echo off
-rd /s /q "%~dp0/build"
-cmake -B "%~dp0/build" -S "%~dp0/Plugin" --preset=build-debug-clang-cl-msvc
