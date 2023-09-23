feb4ee11d4a402c80f07b5f00df6bbe6363b7b5c
vcpkg-action <gottyduke@hotmail.com>
2023-09-23 15:38:25 +0800

    feat: update scripts from `main`

--- build-release.ps1
+++
@@ -1,3 +0,0 @@
-Remove-Item $PSScriptRoot/build -Recurse -Force -ErrorAction:SilentlyContinue -Confirm:$False | Out-Null
-& cmake -B $PSScriptRoot/build -S $PSScriptRoot/Plugin --preset=REL
-& cmake --build $PSScriptRoot/build --config Release
